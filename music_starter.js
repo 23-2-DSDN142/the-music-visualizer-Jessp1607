a7f0a4037b8029f1e04b893f4f3dc2e91d25cdbd
Jessp1607 <pateljess2@myvuw.ac.nz>
2023-10-20 01:04:27 +1300

    equalizer

--- music_starter.js
+++ music_starter.js
@@ -1,6 +1,6 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  colourMode(HSB, 100);
+  colorMode(HSB, 100);
   background(255);
   rectMode(CENTER);
   strokeWeight(9)
@@ -10,13 +10,15 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   var lengthOfLine = 300;
   var LineStart = 100;
   var lineEnd = LineStart+lengthOfLine;
-  line(LineStart, 300, lineEnd, 300);
 
-  
-  
-  
-  
-  
+
+  for(var i = 1; i < drumMap; i++){
+   let lineStep = i*10 
+   line(LineStart, lineStep, lineEnd, lineStep);
+   
+  }
+
+
    //if(bass < 20){
   // background(20)}
   // else{
